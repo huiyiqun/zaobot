025c7388ba4c70eaee953538f67602cedcee5f81
惠轶群 <huiyiqun@gmail.com>
2015-09-29 21:36:01 +0800

    supported for /bug and /bot

--- zao.py
+++ zao.py
@@ -1,7 +1,7 @@
 import telebot
 import logging
 import threading
-from datetime import datetime
+from datetime import datetime, timedelta
 
 telebot.logger.setLevel(logging.DEBUG)
 
@@ -16,8 +16,16 @@ def get_today():
 today = get_today()
 waken_guys = []
 lock = threading.Lock()
-
+stop = False
+stop_util = None
 bot = telebot.TeleBot(readfile('token.txt'))
+
+@bot.message_handler(func=lambda message: stop)
+def sleep(message):
+    global stop_util, stop
+    if stop_util > datetime.now():
+        stop = False
+        send_message(message.chat.id, "<(=ㄒ﹏ㄒ=)> 终于从臭水沟里爬出来了")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -38,5 +46,16 @@ def zao(message):
             else:
                 bot.reply_to(message, "你是第{:d}起床的少年".format(len(waken_guys)+1))
             waken_guys.append(message.from_user.id)
+
+@bot.message_handler(commands=['bug'])
+def bug(message):
+    global stop
+    stop = True
+    stop_utils = datetime.now() + timedelta(minutes=5)
+    bot.reply_to(message, "烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫烫...")
+
+@bot.message_handler(commands=['bot'])
+def hands_up(message):
+    bot.reply_to(message, "<(=°ο°=)> 出现了，这是一只圈养的bot")
 
 bot.polling()
