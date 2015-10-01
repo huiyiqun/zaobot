858132312b4f33a4bb8387f948c0388470cf7ab7
惠轶群 <huiyiqun@gmail.com>
2015-10-01 16:14:16 +0800

    update plugin zao: allow list zao guys

--- plugins/zao.py
+++ plugins/zao.py
@@ -11,17 +11,37 @@ class ZaoBot(TimerBot):
         self.lock = Lock()
         super().__init__(*args, **kwargs)
 
+    def _list_guys(self):
+        '''
+        return list of guys as tuple(`name`, `time`) which is sorted by date
+        '''
+        return sorted(self.waken_guys.values())
+
     def bind(self):
         @self.sched.scheduled_job('cron', hour='3')
         def clear_guys():
             self.waken_guys = dict()
+
+        @self.bot.message_handler(commands=['zaoguys'])
+        def list_guys(message):
+            sorted_guys = self._list_guys()
+            if sorted_guys:
+                self.bot.reply_to(
+                        message,
+                        '\n'.join(map(lambda guy: str(guy[0]) + ': ' + guy[1], sorted_guys)))
+            else:
+                self.bot.reply_to(message, 'o<<(≧口≦)>>o 还没人起床')
         
         @self.bot.message_handler(commands=['zao'])
         def bug(message):
             with self.lock:
                 index = len(self.waken_guys)
                 if message.from_user.id not in self.waken_guys:
-                    self.waken_guys[message.from_user.id] = datetime.now()
+                    if message.from_user.last_name is None:
+                        name = message.from_user.first_name
+                    else:
+                        name = message.from_user.first_name + ' ' + message.from_user.last_name
+                    self.waken_guys[message.from_user.id] = (datetime.now(), name)
                     rewaken = False
                 else:
                     rewaken = True
